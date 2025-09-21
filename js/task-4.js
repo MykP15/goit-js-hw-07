d0252052f7c0c878f8124a2fef2cbd7f8fd147f2
Mykhailo Pyroh <mykhailopk@gmail.com>
2025-09-21 19:48:08 +0200

    first

---
+++ js/task-4.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit)
+function handleSubmit(event){
+    event.preventDefault();
+    const form = event.target;
+    const login = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+
+    if (login === "" || password === "") {
+        console.log("All form fields must be filled in")
+        return;
+    }
+
+        console.log(`Login: ${login}, Password: ${password}`);
+        form.reset();
+  
+}
